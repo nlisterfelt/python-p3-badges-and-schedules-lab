1af3fa017b2500e4e3c00a37681ea35dc69c8e2f
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-27 10:33:09 -0500

    comprehension lists

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,16 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {names.index(name)+1}!" for name in names]
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
